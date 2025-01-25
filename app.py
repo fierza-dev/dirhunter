2818c2a1f49807bd297eba6cf4e9c1e6b87aa7e3
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-01-26 03:51:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,29 +7,10 @@ from lib.host import is_host_reachable
 from lib.load_wordlist import load_wordlist
 from lib.load_useragents import load_useragents
 from imports.common_import import exit,gather,run
-
 async def main():
-    display_banner()
-
-    args = parse_arguments()
-
-    scan_data = ScanData(host=args.url, wordlist_path=args.wordlist, useragent_file=args.useragent)
-    
-    if not is_host_reachable(scan_data.host):
-        print(f'[!] Host {scan_data.host} is not reachable. Exiting.')
-        exit(1)
-
-    await gather(
-        load_wordlist(scan_data),
-        load_useragents(scan_data)
-    )
-
-    scanner = Scanner(scan_data)
-    await scanner.run_scan()
-
-if __name__ == '__main__':
-    try:
-        run(main())
-    except KeyboardInterrupt:
-        print('\n[!] Scan interrupted. Goodbye!')
-        exit(1)
+	display_banner();OO00O000O0OOO0000=parse_arguments();OO000O0O000OOOOOO=ScanData(host=OO00O000O0OOO0000.url,wordlist_path=OO00O000O0OOO0000.wordlist,useragent_file=OO00O000O0OOO0000.useragent)
+	if not is_host_reachable(OO000O0O000OOOOOO.host):print(f"[!] Host {OO000O0O000OOOOOO.host} is not reachable. Exiting.");exit(1)
+	await gather(load_wordlist(OO000O0O000OOOOOO),load_useragents(OO000O0O000OOOOOO));O00O00000OOO00O00=Scanner(OO000O0O000OOOOOO);await O00O00000OOO00O00.run_scan()
+if __name__=='__main__':
+	try:run(main())
+	except KeyboardInterrupt:print('\n[!] Scan interrupted. Goodbye!');exit(1)
