42f03a0d2666b915f06d20566afb9baa4cfdd8a9
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-01-26 03:52:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from lib.load_wordlist import load_wordlist
 from lib.load_useragents import load_useragents
 from imports.common_import import exit,gather,run
 async def main():
-	display_banner();OO00O000O0OOO0000=parse_arguments();OO000O0O000OOOOOO=ScanData(host=OO00O000O0OOO0000.url,wordlist_path=OO00O000O0OOO0000.wordlist,useragent_file=OO00O000O0OOO0000.useragent)
+	display_banner();OO00O000O0OOO0000=parse_arguments();OO000O0O000OOOOOO=ScanData(host=OO00O000O0OOO0000.url,wordlist_path=OO00O000O0OOO0000.wordlist,useragent_file=OO00O000O0OOO0000.user_agent)
 	if not is_host_reachable(OO000O0O000OOOOOO.host):print(f"[!] Host {OO000O0O000OOOOOO.host} is not reachable. Exiting.");exit(1)
 	await gather(load_wordlist(OO000O0O000OOOOOO),load_useragents(OO000O0O000OOOOOO));O00O00000OOO00O00=Scanner(OO000O0O000OOOOOO);await O00O00000OOO00O00.run_scan()
 if __name__=='__main__':
