8b22cb3976811d4daa2759a11594ae252a23d811
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-02 11:31:17 +0700

    Update args.py

--- lib/args.py
+++ lib/args.py
@@ -1,9 +1,15 @@
-from imports.common_import import Path,ArgumentParser
+from imports.common_import import Path, ArgumentParser
 
 def parse_arguments():
-    parser = ArgumentParser(description="Directory Scanner")
-    parser.add_argument('-u', '--url', type=str, required=True, help='Target URL')
-    parser.add_argument('-wl', '--wordlist', type=Path,help='Custom wordlist path')
+    parser = ArgumentParser(
+        usage="%(prog)s -u <URL> [options]",
+    )
+    
+    parser.add_argument('-u', '--url', type=str, help='Target URL')
+    parser.add_argument('-v', '--version', action='store_true', help='Show the version of Dirhunter')
+    parser.add_argument('-wl', '--wordlist', type=Path, help='Custom wordlist file path')
     parser.add_argument('-ua', '--user-agent', type=Path, help='Custom User-Agent file path')
-    parser.add_argument('-ws', '--webshell', help='Searching for webshell files in the web directory (Next Update)')
+    parser.add_argument('--threads', type=int, help="Custom Speed Scanning Using Threads, e.g., 50 (min) to 3500 (max)")
+    parser.add_argument('-t','--time-out', type=int, help="Custom Time Out Scanning, e.g., 10 (deafult)")
+    
     return parser.parse_args()
