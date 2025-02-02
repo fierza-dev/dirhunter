d1a159560fa5f737c927571862b5fe3b6f447121
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-02 11:27:47 +0700

    Update app.py
    Update To 2.2 (Cheetah Booster)

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-from lib.scanner import Scanner
-from lib.scandata import ScanData
-from lib.args import parse_arguments
-from lib.banner import display_banner
-from lib.host import is_host_reachable
-from lib.load_wordlist import load_wordlist
-from lib.load_useragents import load_useragents
-from imports.common_import import exit,gather,run
+_A='bold'
+from imports.app_import import*
+from imports.common_import import exit,gather,run,Console
 async def main():
-	display_banner();OO00O000O0OOO0000=parse_arguments();OO000O0O000OOOOOO=ScanData(host=OO00O000O0OOO0000.url,wordlist_path=OO00O000O0OOO0000.wordlist,useragent_file=OO00O000O0OOO0000.user_agent)
-	if not is_host_reachable(OO000O0O000OOOOOO.host):print(f"[!] Host {OO000O0O000OOOOOO.host} is not reachable. Exiting.");exit(1)
-	await gather(load_wordlist(OO000O0O000OOOOOO),load_useragents(OO000O0O000OOOOOO));O00O00000OOO00O00=Scanner(OO000O0O000OOOOOO);await O00O00000OOO00O00.run_scan()
+	display_banner();args=parse_arguments()
+	if args.version:Console.print('[-] Version : 2.2 (Cheetah Booster)',style=_A);exit(1)
+	scan_data=ScanData(host=args.url,wordlist_path=args.wordlist,useragent_file=args.user_agent,threads=args.threads,timeout=args.time_out)
+	if not await is_host_reachable(scan_data.host):Console.print(f"[bold red1][!][/bold red1] Host {scan_data.host} is not reachable. Exiting.",style=_A);exit(1)
+	await gather(load_wordlist(scan_data),load_useragents(scan_data));scanner=Scanner(scan_data);await scanner.run_scan()
 if __name__=='__main__':
 	try:run(main())
-	except KeyboardInterrupt:print('\n[!] Scan interrupted. Goodbye!');exit(1)
+	except KeyboardInterrupt:Console.print('\n[bold red1][!][/bold red1] Scan interrupted. Goodbye!',style=_A);exit(1)
