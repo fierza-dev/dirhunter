5a84b7ace5333d67b446db0c623b86286e9b5d6b
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-02 11:32:25 +0700

    Update common_import.py

--- imports/common_import.py
+++ imports/common_import.py
@@ -1,42 +1,19 @@
+_B='asyncio'
+_A='timeout'
 from sys import exit
-from re import compile
+from re import compile,match
 from pathlib import Path
-from itertools import cycle
+from random import choice
 from dotenv import load_dotenv
-from colorama import Fore,Style
 from os import system,name,getenv
 from argparse import ArgumentParser
 from aiofiles import open as aio_open
-from asyncio import Semaphore,gather,run
+from asyncio import Semaphore,gather,run,TimeoutError,create_task
 from aiohttp import ClientTimeout,ClientError,ClientSession
-from socket import create_connection,timeout,setdefaulttimeout
-
-__all__ = [
-    "sys",
-    "run",
-    "Path",
-    "exit",
-    "Fore",
-    "name",
-    "cycle",
-    "Style",
-    "gather",
-    "system",
-    "getenv",
-    "socket",
-    "timeout",
-    "compile",
-    "asyncio", 
-    "asyncio",
-    "aiohttp",
-    "aio_open",
-    "aiofiles",
-    "Semaphore",
-    "load_dotenv",
-    "ClientError",
-    "ClientTimeout",
-    "ClientSession",
-    "ArgumentParser",
-    "setdefaulttimeout",
-    "create_connection",
-]
+from socket import create_connection,timeout,setdefaulttimeout,error
+from urllib.parse import urlparse
+from cachetools import cached,TTLCache
+from rich.console import Console
+from async_timeout import timeout
+Console=Console()
+__all__=['sys','run','Path','exit','name','match','error','cached','choice','gather','system','getenv','socket',_A,_A,'compile',_B,_B,'aiohttp','Console','TTLCache','aio_open','aiofiles','urlparse','Semaphore','create_task','load_dotenv','ClientError','TimeoutError','ClientTimeout','ClientSession','ArgumentParser','setdefaulttimeout','create_connection']
