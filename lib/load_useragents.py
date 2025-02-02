2f4ce371fd8db841f5e89993a8b29b8a039ecca8
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-02 11:29:12 +0700

    Update load_useragents.py

--- lib/load_useragents.py
+++ lib/load_useragents.py
@@ -1,10 +1,12 @@
-from imports.common_import import Fore,Style
+from imports.common_import import aio_open,Console
 
 async def load_useragents(scan_data):
     try:
-        scan_data.useragents = scan_data.useragent_file
-        scan_data.set_random_useragent()
-        print(f'[+] User-Agent list loaded: {len(scan_data.useragents)} user agents')
-    except FileNotFoundError:
-        print(f'[{Fore.YELLOW}Warning{Style.RESET_ALL}] User-Agent file not found: {scan_data.useragent_file}. Using default User-Agent.')
-        scan_data.set_random_useragent()
+        async with aio_open(scan_data.useragent_file, mode='r') as file:
+            async for line in file:
+                scan_data.useragents.append(line.strip())
+        Console.print(f'[+] User-Agent list loaded: {sum(1 for _ in scan_data.useragents)} user agents', style="bold")
+
+    except (OSError, FileNotFoundError):
+        Console.print(f'[ Warning ] User-Agent file not found: {scan_data.useragent_file}. Using default User-Agent.', Style="yellow")
+        scan_data.useragents = ["Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"]
