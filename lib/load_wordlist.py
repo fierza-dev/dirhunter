4d02a34af669838c3d5ceaa9e77d78d21b27c697
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-02 11:28:55 +0700

    Update load_wordlist.py

--- lib/load_wordlist.py
+++ lib/load_wordlist.py
@@ -1,10 +1,13 @@
-from imports.common_import import exit,aio_open,Fore,Style
+from imports.common_import import exit, aio_open,Console
 
 async def load_wordlist(scan_data):
     try:
         async with aio_open(scan_data.wordlist_path, mode='r') as file:
-            scan_data.paths = (await file.read()).strip().splitlines()
-        print(f'[+] Wordlist loaded: {scan_data.wordlist_path} ({len(scan_data.paths)} paths)')
+            scan_data.paths = [line.strip() async for line in file] # Batasi jumlah path jika perlu
+        Console.print(f'[+] Wordlist loaded: {scan_data.wordlist_path} ({sum(1 for _ in scan_data.paths)} paths)', style="bold")
     except FileNotFoundError:
-        print(f'[{Fore.RED}Error{Style.RESET_ALL}] Wordlist not found: {scan_data.wordlist_path}')
+        Console.print(f'[Error] Wordlist not found: {scan_data.wordlist_path}', style="bold red")
+        exit(1)
+    except PermissionError:
+        Console.print(f'[Error] Permission denied: {scan_data.wordlist_path}', style="bold red")
         exit(1)
