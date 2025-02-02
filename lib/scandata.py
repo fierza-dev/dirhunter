23af05a2792e7a9291da62ffebd084a64c84150b
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-02 11:28:39 +0700

    Update scandata.py

--- lib/scandata.py
+++ lib/scandata.py
@@ -1,16 +1,14 @@
-from imports.common_import import getenv,cycle,load_dotenv
+from imports.common_import import getenv, load_dotenv
 
 load_dotenv()
 
 class ScanData:
-    def __init__(self, host, wordlist_path=None, useragent_file=None):
+    def __init__(self, host, wordlist_path=None, useragent_file=None, threads=None, timeout=None):
         self.host = host
         self.wordlist_path = wordlist_path or getenv('wordlist')
         self.useragent_file = useragent_file or getenv('useragents')
+        self.threads = threads or 3500
+        self.timeout = timeout or 10
         self.paths = []
         self.results = []
         self.useragents = []
-        self.current_useragent = None
-
-    def set_random_useragent(self):
-        self.current_useragent = next(cycle(self.useragents)) if self.useragents else next(cycle(self.useragent_file))
