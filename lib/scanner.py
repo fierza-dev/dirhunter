daf71f2a99e770e8866a8e27c208a2ea0b2e68b3
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-25 19:01:47 +0700

    Update scanner.py

--- lib/scanner.py
+++ lib/scanner.py
@@ -1,21 +1,9 @@
 from lib.check_path import check_path
-from imports.common_import import gather, ClientSession, Console,create_task
-
+from imports.common_import import gather,ClientSession,Console,create_task
 class Scanner:
-    def __init__(self, config):
-        self.GetConfig = config
-
-    async def limited_check_path(self, session, path):
-        return await check_path(session, self.GetConfig, path)
-    
-    async def run_scan(self):
-        Console.print(f'\n[+] Starting scan on [bold white]{self.GetConfig.host}[/bold white]', style="bold")
-        
-        async with ClientSession() as session:
-            tasks = [create_task(self.limited_check_path(session, path)) for path in self.GetConfig.paths]
-            results = await gather(*tasks, return_exceptions=True)
-
-        self.GetConfig.results.extend([res for res in results if res and not isinstance(res, Exception)])
-
-        Console.print(f'\n[+] PATH : {len(self.GetConfig.results)} valid paths found.', style="bold")
-        Console.print(f"\n[+] Scanning Complete ...", style="bold bright_white")
+	def __init__(self,config):self.GetConfig=config
+	async def limited_check_path(self,session,path):return await check_path(session,self.GetConfig,path)
+	async def run_scan(self):
+		A='bold';Console.print(f"\n[+] Starting scan on [bold white]{self.GetConfig.host}[/bold white]",style=A)
+		async with ClientSession()as session:tasks=[create_task(self.limited_check_path(session,path))for path in self.GetConfig.paths];results=await gather(*tasks,return_exceptions=True)
+		self.GetConfig.results.extend([res for res in results if res and not isinstance(res,Exception)]);Console.print(f"\n[+] PATH : {len(self.GetConfig.results)} valid paths found.",style=A);Console.print(f"\n[+] Scanning Complete ...",style='bold bright_white')
