2a2b89cb231ead25359fdbaac545716f2710b529
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-25 18:30:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-_A='bold'
-from imports.app_import import*
-from imports.common_import import exit,gather,run,Console
-async def main():
-	display_banner();args=parse_arguments()
-	if args.version:Console.print('[-] Version : 2.2 (Cheetah Booster)',style=_A);exit(1)
-	scan_data=ScanData(host=args.url,wordlist_path=args.wordlist,useragent_file=args.user_agent,threads=args.threads,timeout=args.time_out)
-	if not await is_host_reachable(scan_data.host):Console.print(f"[bold red1][!][/bold red1] Host {scan_data.host} is not reachable. Exiting.",style=_A);exit(1)
-	await gather(load_wordlist(scan_data),load_useragents(scan_data));scanner=Scanner(scan_data);await scanner.run_scan()
-if __name__=='__main__':
-	try:run(main())
-	except KeyboardInterrupt:Console.print('\n[bold red1][!][/bold red1] Scan interrupted. Goodbye!',style=_A);exit(1)
+from lib.main import main
+from imports.common_import import exit, run, Console
+
+if __name__ == '__main__':
+    try:
+        run(main())
+    except KeyboardInterrupt:
+        Console.print('\n[bold red1][!][/bold red1] Scan interrupted. Goodbye!', style="bold")
+        exit(1)
