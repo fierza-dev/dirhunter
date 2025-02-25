28d91087302d61aee5f75cf190c85532ddf75241
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-25 18:31:35 +0700

    Update common_import.py

--- imports/common_import.py
+++ imports/common_import.py
@@ -1,19 +1,17 @@
-_B='asyncio'
-_A='timeout'
 from sys import exit
-from re import compile,match
 from pathlib import Path
-from random import choice
+from socket import AF_INET
+from re import compile,match
 from dotenv import load_dotenv
-from os import system,name,getenv
-from argparse import ArgumentParser
-from aiofiles import open as aio_open
-from asyncio import Semaphore,gather,run,TimeoutError,create_task
-from aiohttp import ClientTimeout,ClientError,ClientSession
-from socket import create_connection,timeout,setdefaulttimeout,error
-from urllib.parse import urlparse
-from cachetools import cached,TTLCache
 from rich.console import Console
-from async_timeout import timeout
-Console=Console()
-__all__=['sys','run','Path','exit','name','match','error','cached','choice','gather','system','getenv','socket',_A,_A,'compile',_B,_B,'aiohttp','Console','TTLCache','aio_open','aiofiles','urlparse','Semaphore','create_task','load_dotenv','ClientError','TimeoutError','ClientTimeout','ClientSession','ArgumentParser','setdefaulttimeout','create_connection']
+from os import system,name,getenv
+from random import choice,randint
+from aiodns import DNSResolver,error
+from aiofiles import open as aio_open
+from async_timeout import timeout as asyncTimeout
+from argparse import ArgumentParser,RawTextHelpFormatter
+from aiohttp import ClientTimeout,ClientError,ClientSession
+from asyncio import Semaphore,gather,run,TimeoutError,create_task
+from socket import create_connection,timeout,setdefaulttimeout,error
+
+Console = Console()
