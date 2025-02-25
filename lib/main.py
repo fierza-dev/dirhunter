a06c4eb97c3923ad87fba940c9bcc261bc6270ad
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-25 19:00:04 +0700

    Update main.py

--- lib/main.py
+++ lib/main.py
@@ -1,16 +1,5 @@
-from imports.app_import import *
-
+from imports.app_import import*
 async def main():
-    display_banner()
-
-    args = parse_arguments()
-
-    if args.version:
-        version()
-
-    config = await initialize_Configuration(args)
-    await check_host_reachability(config.host)
-    await load_resources(config)
-
-    scanner = Scanner(config)
-    await scanner.run_scan()
+	display_banner();args=parse_arguments()
+	if args.version:version()
+	config=await initialize_Configuration(args);await check_host_reachability(config.host);await load_resources(config);scanner=Scanner(config);await scanner.run_scan()
