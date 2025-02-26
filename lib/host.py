975cdcfeba463971715f3cf2abbaccf7e8acfbbb
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-26 08:16:40 +0700

    Update host.py

--- lib/host.py
+++ lib/host.py
@@ -1,12 +1,11 @@
-from imports.common_import import DNSResolver,error,AF_INET,Console
+from imports.common_import import urlparse,Console
 
 async def is_host_reachable(url):
-    resolver = DNSResolver()
-
-    try:
-        await resolver.gethostbyname(url, AF_INET)
-        Console.print(f"[[bold green]✓[/bold green]] Host: [white]{url}[/white]", style="bold")
-        return True
-    except (error.DNSError, OSError) as e:
-        Console.print(f"[[bold red]✗[/bold red]] Host tidak ditemukan atau DNS gagal: [white]{url}[/white]", style="bold")
-        return False
+    parsed_url = urlparse(url)
+    
+    if not (parsed_url.scheme and parsed_url.netloc):
+        Console.print(f"[[bold red1]x[/bold red1]] Invalid URL format: {url}", style="bold")
+        return None
+    
+    Console.print(f"[[bold sea_green1]✓[/bold sea_green1]] Host: {parsed_url.netloc}", style="bold bright_white")
+    return True
