8544c484e953d63330c8293785635eca471075b5
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-02-26 08:12:51 +0700

    Update host.py

--- lib/host.py
+++ lib/host.py
@@ -7,6 +7,6 @@ async def is_host_reachable(url):
         await resolver.gethostbyname(url, AF_INET)
         Console.print(f"[[bold green]✓[/bold green]] Host: [white]{url}[/white]", style="bold")
         return True
-    except error.DNSError as e:
-        Console.print(f"[[bold red]✗[/bold red]] Host tidak ditemukan: [white]{url}[/white]", style="bold")
+    except (error.DNSError, OSError) as e:
+        Console.print(f"[[bold red]✗[/bold red]] Host tidak ditemukan atau DNS gagal: [white]{url}[/white]", style="bold")
         return False
