8d6e843cc004c1bb8d3b0df609ab6945f10ba2a2
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-03-17 19:29:34 +0700

    Update check_path.py

--- lib/check_path.py
+++ lib/check_path.py
@@ -1,9 +1,17 @@
 import asyncio
 from lib.get_headers import get_headers
 from lib.process_response import process_response
-from imports.common_import import Semaphore,choice,ClientSession
-async def check_path(session,GetConfig,path):
-	A=GetConfig;D=Semaphore(A.threads);B=A.host+f"/{path.lstrip("/")}";E=await get_headers(choice(A.useragents),A.httpSmug);F=choice(A.Getproxy)if A.Getproxy else A.proxy
-	async with D:
-		async with asyncio.timeout(A.timeout):
-			async with session.get(B,headers=E,proxy=F or None,allow_redirects=False)as C:await process_response(C.headers,C.status,B,A)
+from imports.common_import import (
+    Semaphore, choice, ClientSession
+)
+
+async def check_path(session: ClientSession, GetConfig, path):
+    semaphore = Semaphore(GetConfig.threads)
+    url = GetConfig.host + f'/{path.lstrip("/")}'
+    headers = await get_headers(choice(GetConfig.useragents), GetConfig.httpSmug)
+    proxy = choice(GetConfig.Getproxy) if GetConfig.Getproxy else GetConfig.proxy
+
+    async with semaphore:
+        async with asyncio.timeout(GetConfig.timeout):
+            async with session.get(url, headers=headers, proxy=proxy or None, allow_redirects=False) as response:
+                await process_response(response.headers,response.status, url, GetConfig)
