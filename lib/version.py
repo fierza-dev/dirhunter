be6bb36513668c7da48685c6c9d12b1c95238c2c
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-03-17 18:44:53 +0700

    Update version.py

--- lib/version.py
+++ lib/version.py
@@ -1,5 +1,5 @@
 from imports.common_import import exit, Console
 
 def version():
-    Console.print("[-] Version : 2.3 (Zenith Rush)", style="bold")
+    Console.print("[-] Version : 2.3.1", style="bold")
     exit(1)
