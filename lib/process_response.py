7e6e9a26d412be3cf455393d750fcd4ae645a865
Fierza <102312310+fierza-dev@users.noreply.github.com>
2025-03-20 08:54:36 +0700

    Update process_response.py

--- lib/process_response.py
+++ lib/process_response.py
@@ -5,7 +5,7 @@ async def process_response(redirect,status, url, scan_data):
         if status == 200:
             Console.print(f"[[bold sea_green1]{status}[/bold sea_green1]] [bold white]{url}[/bold white]")
         elif status in (301, 302, 303, 307, 308):
-            Console.print(f"[[bold magenta]{status}[/bold magenta]] [white]{url}[white] -> [bold white]{redirect.get("Location")}[/bold white]")
+            Console.print(f"[[bold magenta]{status}[/bold magenta]] [white]{url}[white] -> [bold white]{redirect.get('Location')}[/bold white]")
         else:
             Console.print(f"[[bold red1]{status}[/bold red1]] [bold white]{url}[/bold white]")
         scan_data.results.append(url)
